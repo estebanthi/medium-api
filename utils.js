fd40d25ac8531ce9e4fff21e04aca046d97446f4
estebanthi <esteban.thilliez@gmail.com>
2023-06-26 18:34:49 +0200

    vercel

--- utils.js
+++ utils.js
@@ -4,6 +4,7 @@ const constants = require('./const')
 const formatMediumResponse = (response) => {
     const body = response.body
     const strippedBody = body.substring(constants.mediumResPrefix.length)
+    console.log(strippedBody)
     const parsedBody = JSON.parse(strippedBody)
     return parsedBody
 }
